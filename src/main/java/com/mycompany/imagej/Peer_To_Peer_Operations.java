16d8690dc6e1d8ff585e37c0d204430522ae1b4b
rcferreira232 <rafaelcferreira232@gmail.com>
2026-05-28 18:40:18 -0300

    Add initial implementations for Convert_Rgb_To_Grey and Peer_To_Peer_Operations plugins

---
+++ src/main/java/com/mycompany/imagej/Peer_To_Peer_Operations.java
@@ -0,0 +1,11 @@
+package com.mycompany.imagej;
+
+// Criar um plugin para alterar os valores de brilho e contraste de uma imagem, bem como aplicar as técnicas de solarização e dessaturação na  mesma.
+
+// Utilizar uma interface gráfica com quatro barras do tipo slider, uma para cada técnica,  um botão "ok" e um botão "cancel".
+// As barras de slider alterarão as características da imagem quando movimentadas.
+// Quando pressionado o botão "ok" as características da imagem serão alteradas de forma definitiva, caso seja pressionado o botão "cancel" a imagem voltará para as suas caraterísticas originais.
+
+public class Peer_To_Peer_Operations {
+    
+}
